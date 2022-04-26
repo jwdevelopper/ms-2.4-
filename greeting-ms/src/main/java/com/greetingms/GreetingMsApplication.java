c7860c7178e0caea5dfafddf393c75cefdbf5476
José Wilson Junior <jwdevelopper@gmail.com>
2022-04-26 16:53:57 -0300

    Inicio repo

---
+++ greeting-ms/src/main/java/com/greetingms/GreetingMsApplication.java
@@ -0,0 +1,13 @@
+package com.greetingms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingMsApplication.class, args);
+	}
+
+}
---
+++ greeting-ms/src/test/java/com/greetingms/GreetingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greetingms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
