33af062981b470f9b0aa670db000b69e578f6361
José Wilson Junior <jwdevelopper@gmail.com>
2022-05-04 16:54:52 -0300

    impl gateway api

---
+++ cambio-service/src/test/java/com/br/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
